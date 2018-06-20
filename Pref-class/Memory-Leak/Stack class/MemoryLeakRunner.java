922daa1905a2fc7de9bb19c8df3202f9e366a6ea
Asaf Samuel <asafsamuel100@gmail.com>
2018-06-20 14:25:07 +0300

    Add files via upload

--- Pref-class/Memory-Leak/Stack class/MemoryLeakRunner.java
+++ Pref-class/Memory-Leak/Stack class/MemoryLeakRunner.java
@@ -6,23 +6,60 @@ public class MemoryLeakRunner
 {
 	public static void main(String[] args) 
 	{
-		final double iterNum = 1E7;	// change it depends on your computer
-		final int initSize = 100;
+		final int numStacks = 100;
+		final int numElementsPerStack = 10_000_000;
+		Stack<Integer>[] stacks = (Stack<Integer>[]) new Stack[numStacks];
 		
-		Stack<Object> stack = new Stack<>(initSize);
-		Stack<Object> stack2 = new Stack<>(initSize);
+		for(int i=0; i<numStacks; i++)
+			stacks[i] = new Stack<Integer>(1000);
 		
-		for(int i=0; i<iterNum; i++)
+		for(int i=0; i<numStacks; i++)
 		{
-			stack.put(new Object());
-			stack2.put(new Object());
+			for(int j=0; j<numElementsPerStack; j++)
+				stacks[i].put(j);
+					
+			System.out.println("finished putting " +stacks[i].size() + " elements in stack #"+i);
+			
+			for(int j=0; j<numElementsPerStack; j++)
+				stacks[i].pop();
+			
+			System.out.println("Stack #"+i+" size after popping "+stacks[i].size());
 		}
 		
-		for(int i=0; i<iterNum; i++)
-		{
-			stack.pop();
-			stack2.pop();
-		}
+		/*
+		 * finished putting 10000001 elements in stack #0
+		   Stack #0 size after popping 1
+		 * finished putting 10000001 elements in stack #1
+		   Stack #1 size after popping 1
+		 * finished putting 10000001 elements in stack #2
+		   Stack #2 size after popping 1
+		 * finished putting 10000001 elements in stack #3
+		   Stack #3 size after popping 1
+		 * finished putting 10000001 elements in stack #4
+		   Stack #4 size after popping 1
+		 * finished putting 10000001 elements in stack #5
+		   Stack #5 size after popping 1
+		 * finished putting 10000001 elements in stack #6
+		   Stack #6 size after popping 1
+		 * finished putting 10000001 elements in stack #7
+		   Stack #7 size after popping 1
+		 * finished putting 10000001 elements in stack #8
+		   Stack #8 size after popping 1
+		 * finished putting 10000001 elements in stack #9
+		   Stack #9 size after popping 1
+		 * finished putting 10000001 elements in stack #10
+		   Stack #10 size after popping 1
+		 * finished putting 10000001 elements in stack #11
+		   Stack #11 size after popping 1
+		 * finished putting 10000001 elements in stack #12
+		   Stack #12 size after popping 1
+		 * finished putting 10000001 elements in stack #13
+		   Stack #13 size after popping 1
+		 
+		 * Exception in thread "main" java.lang.OutOfMemoryError: GC overhead limit exceeded
+		 * at java.lang.Integer.valueOf(Unknown Source)
+		 * at iaf.perf.course.day4.Stack.main(Stack.java:56)
+		 */
 		
 		/*** Active HeapDump here (if you want):
 		 * 1) open cmd
@@ -30,7 +67,7 @@ public class MemoryLeakRunner
 		 * 3) look for our process's id
 		 * 4) write "jcmd [id] help"
 		 * 5) write "jcmd [id] GC.heap_dump [output_file.hprof]
-		 * 6) open output_file.hprof
+		 * 6) open output_file.hprof via Java VirtualVM
 		***/
 		try {
 			System.in.read();
